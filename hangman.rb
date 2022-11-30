c8f03108f9543a7f8054cc1d041a5daa8c62cd21
katie <katiepb525@gmail.com>
2022-11-30 11:00:15 -0600

    create method that checks for valid input

--- hangman.rb
+++ hangman.rb
@@ -1,6 +1,14 @@
+module CheckInput
+  # check input contains only uppercase or lowercase letters, and is only 1chr long
+  def input_ok?(input)
+    input.length == 1 && (input.count('^A-Z').zero? || input.count('^a-z').zero?)
+  end
+end
+
 class Player
+  include CheckInput
   attr_reader :user_input
-  
+
   def initalize
     @user_input = ''
   end
@@ -9,3 +17,7 @@ class Player
     @user_input = gets.chomp
   end
 end
+
+player = Player.new
+player.get_user_input
+p player.input_ok?(player.user_input)
